20627d7867e09d25c96943d46c7867473524ab5e
olectech <38139969+olectech@users.noreply.github.com>
2018-10-27 22:39:13 +0200

    Nowe aktywności

---
+++ Java/terminyWaznosci.java
@@ -0,0 +1,13 @@
+package pl.oltek.solek.foodmanagement;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class terminyWaznosci extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terminy_waznosci);
+    }
+}
---
+++ Java/zawartoscLodowki.java
@@ -0,0 +1,13 @@
+package pl.oltek.solek.foodmanagement;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class zawartoscLodowki extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zawartosc_lodowki);
+    }
+}
