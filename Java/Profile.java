5e8ee0b8d9692e8d08921aeeb957ef4162790af4
olectech <38139969+olectech@users.noreply.github.com>
2018-10-27 22:42:34 +0200

    Dodano komentarze.

--- Java/Profile.java
+++ Java/Profile.java
@@ -12,7 +12,6 @@ import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.squareup.picasso.Picasso;
 
 /**
  * Created by solek on 21.10.2018.
@@ -20,15 +19,15 @@ import com.squareup.picasso.Picasso;
 
 public class Profile extends AppCompatActivity implements View.OnClickListener {
 
-    String useremail, username, userphoto;
+    String useremail, username, userphoto; // wyswietlanie info o profilu zmienne
     Uri url;
 
     ImageView profilephoto;
 
-    FirebaseAuth mAuth;
+    FirebaseAuth mAuth; // autoryzacja firebase
 
-    TextView u, e;
-
+    TextView u, e; // wyswietlanie info o profilu textview
+//deklaracja przycisków
     Button dodajbtn;
     Button zawartosctbtn;
     Button listabtn;
@@ -40,24 +39,24 @@ public class Profile extends AppCompatActivity implements View.OnClickListener {
         setContentView(R.layout.profile);
 
         mAuth = FirebaseAuth.getInstance();
-
+// przyłączenie textview z layoutu
         u = (TextView)findViewById(R.id.profileusername);
         e = (TextView)findViewById(R.id.profileuseremail);
-
+// przyłączenie buttonów
         dodajbtn = (Button)findViewById(R.id.dodajbtn);
         zawartosctbtn = (Button)findViewById(R.id.zawartosctbtn);
         listabtn = (Button)findViewById(R.id.listabtn);
         terminybtn = (Button)findViewById(R.id.terminybtn);
         logoutbtn = (Button)findViewById(R.id.logoutbtn);
-
+// przyłączenie zdjęcia profilowego
         profilephoto = (ImageView)findViewById(R.id.profilephoto) ;
-
+// onClick buttony
         dodajbtn.setOnClickListener(this);
         zawartosctbtn.setOnClickListener(this);
         listabtn.setOnClickListener(this);
         terminybtn.setOnClickListener(this);
         logoutbtn.setOnClickListener(this);
-
+// pobranie danych z poprzedniej intencji nazwa, mail, adres zdjecia profilowego
         useremail = getIntent().getStringExtra("useremail");
         username = getIntent().getStringExtra("username");
         userphoto = getIntent().getStringExtra("url");
@@ -69,23 +68,30 @@ public class Profile extends AppCompatActivity implements View.OnClickListener {
     }
 
     @Override
-    public void onClick(View view) {
+    public void onClick(View view) { // wybór akcji w zależności od naciśniętego buttona
         switch (view.getId()) {
             case R.id.dodajbtn:
-                Intent dodaj = new Intent(this, MojaLodowka.class); // nowa intencja
+                Intent dodaj = new Intent(this, MojaLodowka.class); // nowa intencja dodawanie produktów do bazy
+                dodaj.putExtra("us", useremail);
                 startActivity(dodaj);
                 Toast.makeText(this,"Dodaj?", Toast.LENGTH_LONG).show();
                 break;
             case R.id.zawartosctbtn:
+                Intent zawartosc = new Intent(this, zawartoscLodowki.class); // nowa intencja wykaz produktów w lodowce
+                startActivity(zawartosc);
                 Toast.makeText(this,"Zawartosc?", Toast.LENGTH_LONG).show();
                 break;
             case R.id.listabtn:
+                Intent lista = new Intent(this, listaZakupow.class); // nowa intencja lista zakupów
+                startActivity(lista);
                 Toast.makeText(this,"Lista?", Toast.LENGTH_LONG).show();
                 break;
             case R.id.terminybtn:
+                Intent terminy = new Intent(this, terminyWaznosci.class); // nowa intencja terminy wazności
+                startActivity(terminy);
                 Toast.makeText(this,"Terminy?", Toast.LENGTH_LONG).show();
                 break;
-            case R.id.logoutbtn:
+            case R.id.logoutbtn: // wylogowanie z aplikacji
                 mAuth.signOut();
                 FirebaseAuth.getInstance().signOut();
                 Intent logout = new Intent(this, MainActivity.class);
