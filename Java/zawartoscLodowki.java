42f22188cfa388378df46c3b8658007fd1391f30
olectech <38139969+olectech@users.noreply.github.com>
2018-10-30 23:42:22 +0100

    update

--- Java/zawartoscLodowki.java
+++ Java/zawartoscLodowki.java
@@ -1,16 +1,25 @@
 package pl.oltek.solek.foodmanagement;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.GridView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class zawartoscLodowki extends AppCompatActivity {
 
-    //DatabaseReference databaseProdukty;
+    ArrayList<Produkt> produktArrayList;
+
+
+    DatabaseReference databaseProdukty;
     GridView gridview;
 
     String [] values = {
@@ -69,14 +78,32 @@ public class zawartoscLodowki extends AppCompatActivity {
 
         gridview = (GridView)findViewById(R.id.gridview);
 
+        produktArrayList = new ArrayList<>();
 
         GridAdapter gridAdapter = new GridAdapter(this, images, values);
 
         gridview.setAdapter(gridAdapter);
-        //String nazwaBazy;
-        //nazwaBazy = getIntent().getStringExtra("us").replace("@", "-").replace(".", "-");
+        String nazwaBazy;
+        nazwaBazy = getIntent().getStringExtra("us").replace("@", "-").replace(".", "-");
         //Toast.makeText(this, nazwaBazy, Toast.LENGTH_LONG).show();
-        //databaseProdukty = FirebaseDatabase.getInstance().getReference(nazwaBazy+"/Produkty");
+        databaseProdukty = FirebaseDatabase.getInstance().getReference(nazwaBazy+"/Produkty");
+
+        databaseProdukty.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                produktArrayList.clear();
+                for(DataSnapshot produkty_snaphot : dataSnapshot.getChildren()){
+                    Produkt produkt = produkty_snaphot.getValue(Produkt.class);
+
+                    produktArrayList.add(produkt);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
 
 
     }
