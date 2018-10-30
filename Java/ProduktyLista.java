fe986c40e4e1dc70fea8cda24a2f83daddcb84f4
olectech <38139969+olectech@users.noreply.github.com>
2018-10-30 23:39:41 +0100

    ListaZakupow/Add
    Działająca lista/wyświetlanie z bazy.

---
+++ Java/ProduktyLista.java
@@ -0,0 +1,41 @@
+package pl.oltek.solek.foodmanagement;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class ProduktyLista extends ArrayAdapter<Produkt> {
+    private Activity context;
+    private List<Produkt> produktList;
+
+    public ProduktyLista(Activity context, List<Produkt> produktList){
+            super(context, R.layout.list_layout, produktList);
+            this.context = context;
+            this.produktList = produktList;
+    }
+
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+        View listViewItem = inflater.inflate(R.layout.list_layout, null, true);
+
+        TextView textViewNazwa = (TextView)listViewItem.findViewById(R.id.nazwaProdukt);
+        TextView textViewData = (TextView)listViewItem.findViewById(R.id.dataProdukt);
+
+        Produkt produkt = produktList.get(position);
+
+        textViewData.setText(produkt.produktData);
+        textViewNazwa.setText(produkt.produktNazwa);
+
+        return listViewItem;
+    }
+}
--- Java/listaZakupow.java
+++ Java/listaZakupow.java
@@ -1,13 +1,64 @@
 package pl.oltek.solek.foodmanagement;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class listaZakupow extends AppCompatActivity {
+
+    ListView listViewProdukty;
+    List<Produkt> produktList;
+
+    DatabaseReference databaseProdukty;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_zakupow);
+
+        listViewProdukty = (ListView)findViewById(R.id.listViewProdukty);
+        produktList = new ArrayList<>();
+
+        String nazwaBazy = "NONAME";
+        nazwaBazy = getIntent().getStringExtra("us").replace("@", "-").replace(".", "-");
+        Toast.makeText(this, nazwaBazy, Toast.LENGTH_LONG).show();
+        databaseProdukty = FirebaseDatabase.getInstance().getReference(nazwaBazy+"/Produkty");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        databaseProdukty.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                produktList.clear();
+                for(DataSnapshot produkty_snaphot : dataSnapshot.getChildren()){
+                    Produkt produkt = produkty_snaphot.getValue(Produkt.class);
+
+                    produktList.add(produkt);
+
+                }
+                ProduktyLista adapter = new ProduktyLista(listaZakupow.this, produktList);
+                listViewProdukty.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+
     }
 }
